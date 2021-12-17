2d2cc191240b565a6cbd9e6602b44f3d0d8fe8dd
Fernando dos Santos <nando4803@gmail.com>
2021-12-17 23:18:01 +0100

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Montserrat', 'sans-serif'],
+      'mono': ['Fira Code']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
